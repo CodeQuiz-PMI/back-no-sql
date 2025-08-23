7d644441a012f52cd8efe44decc85c60a1e40f28
DiegoAndreLeffa <diegodocanto@gmail.com>
2025-08-23 17:33:20 -0300

    feat: alterations

--- src/__tests__/controllers.user.test.ts
+++ src/__tests__/controllers.user.test.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import {
   getAllUserController,
@@ -30,11 +30,13 @@ describe("Controlador de Usuário", () => {
   let statusMock: jest.Mock;
   let jsonMock: jest.Mock;
   let sendMock: jest.Mock;
+  let next: NextFunction;
 
   beforeEach(() => {
     statusMock = jest.fn().mockReturnThis();
     jsonMock = jest.fn();
     sendMock = jest.fn();
+    next = jest.fn();
 
     req = { params: {}, body: {} };
     res = {
@@ -57,7 +59,7 @@ describe("Controlador de Usuário", () => {
       const mockUsers = [{ id: "1" }, { id: "2" }];
       (User.find as jest.Mock).mockResolvedValue(mockUsers);
 
-      await getAllUserController(req as Request, res as Response);
+      await getAllUserController(req as Request, res as Response, next);
 
       expect(User.find).toHaveBeenCalled();
       expect(statusMock).toHaveBeenCalledWith(201);
